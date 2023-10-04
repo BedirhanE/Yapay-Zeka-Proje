382f5db5ec259942443e2284e056edc10935e2f3
Bedirhan Elçin <79866900+BedirhanE@users.noreply.github.com>
2023-10-04 20:49:02 +0300

    Artificial Intelligence
    Yapay zeka ya giriş projesi olarak yapmış olduğum yüz tanıma projem

---
+++ yüz tanıma uygulaması/import cv2.py
@@ -0,0 +1,28 @@
+import cv2
+import pyttsx3  # Sesli geri bildirim için pyttsx3 kütüphanesi kullanılıyor
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+
+# Sesli geri bildirim için motor oluştur
+engine = pyttsx3.init()
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        
+        # Yüz tespit edildiğinde sesli geri bildirim
+        engine.say("Bir yüz tespit edildi!")
+        engine.runAndWait()
+
+    cv2.imshow('Yüz Tanıma', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
